234d6242ff4c66275423506229e793feee53270f
Jason Lunsford <miyamota@yahoo.com>
2014-01-20 07:58:47 -0500

    rest of the files, first commit

---
+++ public/js/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('HomeController', function ($scope, $http) {
+
+
+  });
---
+++ startServer.js
@@ -0,0 +1,51 @@
+
+/*********************
+ * Module dependencies
+ *********************/
+
+var express = require('express'),
+  routes = require('./routes'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+app.engine('html', require('ejs').renderFile);
+
+/***************
+ * Configuration
+ ***************/
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+// development only
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+}
+
+/********
+ * Routes
+ ********/
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+/**************
+ * Start Server
+ **************/
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Server listening on port ' + app.get('port'));
+});
